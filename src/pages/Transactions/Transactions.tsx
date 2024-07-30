63a3e27c94bdce6a3b9972a15117f52ea0474704
jandersonvb <jandersonvb.dev@gmail.com>
2024-07-30 16:40:27 -0300

    feat: create component Summary

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,9 +1,12 @@
 import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/Summary";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+
+      <Summary />
     </div>
   );
 }
