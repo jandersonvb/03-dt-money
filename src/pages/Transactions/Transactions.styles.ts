171532651906bdba8160edd231f18f0b8f4e7357
jandersonvb <jandersonvb.dev@gmail.com>
2024-07-30 20:18:12 -0300

    feat: create transactions table on the page Transactions

---
+++ src/pages/Transactions/Transactions.styles.ts
@@ -0,0 +1,48 @@
+import { styled } from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin-top: 1.5rem;
+
+  th {
+    color: var(--text-body);
+    font-weight: 400;
+    padding: 1rem 2rem;
+    text-align: left;
+    line-height: 1.5rem;
+  }
+
+  td {
+    background: ${({ theme }) => theme["gray-700"]};
+    padding: 1.25rem 2rem;
+    
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHightlightProps {
+  variant?: "income" | "outcome";
+}
+
+export const PriceHightlight = styled.span<PriceHightlightProps>`
+  color: ${({ theme, variant }) => variant === "income" ? theme["green-300"] : theme["red-300"]};
+`;
--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,12 +1,36 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
+import { PriceHightlight, TransactionsContainer, TransactionsTable } from "./Transactions.styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.000</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>30/07/2024</td>
+            </tr>
+
+            <tr>
+              <td>Aluguel</td>
+              <td>
+                <PriceHightlight variant="outcome">- R$ 1.200</PriceHightlight>
+              </td>
+              <td>Casa</td>
+              <td>01/08/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
