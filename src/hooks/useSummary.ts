a81372be5386d44d2b68cd8168bd759c8051d515
jandersonvb <jandersonvb.dev@gmail.com>
2024-08-07 16:48:21 -0300

    feat: transform summary in useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { useContextSelector } from "use-context-selector";
 import { TransactionsContext } from "../contexts/TransactionsContext";
+import { useMemo } from "react";
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
@@ -7,24 +8,26 @@ export function useSummary() {
   }
   );
 
-  const summary = transactions.reduce(
-    (accumulator, currentValue) => {
-      if (currentValue.type === 'income') {
-        accumulator.income += currentValue.price
-        accumulator.total += currentValue.price
-      } else {
-        accumulator.outcome += currentValue.price
-        accumulator.total -= currentValue.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === 'income') {
+          accumulator.income += currentValue.price
+          accumulator.total += currentValue.price
+        } else {
+          accumulator.outcome += currentValue.price
+          accumulator.total -= currentValue.price
+        }
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  )
+        return accumulator
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0
+      }
+    )
+  }, [transactions])
 
   return summary;
 }
